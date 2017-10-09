08c41a2097cc980a8adb9845030569d9a0b57f83
Steven Kabbes <stevenkabbes@gmail.com>
2017-10-08 19:48:24 -0700

    fix example to break with POST & retry

--- oss/node-proxy-fail/app.js
+++ oss/node-proxy-fail/app.js
@@ -1,8 +1,11 @@
 const http = require('http')
 const url = require('url');
-const { PORT = 3000 } = process.env
+const { PORT = 3000, UP_RESTARTS } = process.env
+let r = parseInt(UP_RESTARTS, 10);
 
 http.createServer((req, res) => {
+  const myR = r++;
+  r++;
   const parts = url.parse(req.url, true);
 
   let delay = 0, id = 0;
@@ -10,6 +13,12 @@ http.createServer((req, res) => {
   if (parts.pathname === '/delay') {
     const { t = 0, id = 0 } = parts.query;
     setTimeout(() => {
+      if (myR % 3 == 0) {
+        res.writeHead(500);
+        res.end(JSON.stringify(parts.query));
+        return;
+      }
+
       res.end(JSON.stringify(parts.query));
     }, t * 1000);
     return;
